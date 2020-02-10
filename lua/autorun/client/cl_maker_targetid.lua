8696ef47087e7b588ed7764ce18875f27535dbb3
Tim Goll <tim@timgoll.de>
2020-02-10 18:56:05 +0100

    switch to reworked targetID system

--- lua/autorun/client/cl_maker_targetid.lua
+++ lua/autorun/client/cl_maker_targetid.lua
@@ -1,37 +1,39 @@
 -- handle looking at sodas
-hook.Add('TTTRenderEntityInfo', 'ttt2_marker_highlight_players', function(data, params)
+hook.Add('TTTRenderEntityInfo', 'ttt2_marker_highlight_players', function(tData)
 	-- while client is setting up, ignore missing MARKER
 	if not MARKER then return end
 
+	local ent = tData:GetEntity()
+
 	-- has to be a player
-	if not data.ent:IsPlayer() then return end
+	if not ent:IsPlayer() then return end
 
 	if LocalPlayer():GetSubRole() ~= ROLE_MARKER then return end
 
 	if GetGlobalBool('ttt_mark_deal_no_damage', false) then
-		params.displayInfo.desc[#params.displayInfo.desc + 1] = {
-			text = LANG.GetTranslation('ttt_marker_player_deal_no_damage'),
-			color = COLOR_RED
-		}
+		tData:AddDescriptionLine(
+			LANG.GetTranslation('ttt_marker_player_deal_no_damage'),
+			COLOR_RED
+		)
 	end
 
 	-- only add further text to marked players
-	if not MARKER_DATA:IsMarked(data.ent) then return end
+	if not MARKER_DATA:IsMarked(ent) then return end
 
 	if GetGlobalBool('ttt_mark_take_no_damage', false) then
-		params.displayInfo.desc[#params.displayInfo.desc + 1] = {
-			text = LANG.GetTranslation('ttt_marker_player_take_no_damage'),
-			color = COLOR_GREEN
-		}
+		tData:AddDescriptionLine(
+			LANG.GetTranslation('ttt_marker_player_take_no_damage'),
+			COLOR_GREEN
+		)
 	end
 
-	params.displayInfo.desc[#params.displayInfo.desc + 1] = {
-		text = LANG.GetTranslation('ttt_marker_player_marked'),
-		color = MARKER.ltcolor
-	}
+	tData:AddDescriptionLine(
+		LANG.GetTranslation('ttt_marker_player_marked'),
+		MARKER.ltcolor
+	)
 
-	params.displayInfo.icon[#params.displayInfo.icon + 1] = {
-		material = MARKER.iconMaterial,
-		color = MARKER.ltcolor
-	}
+	tData:AddIcon(
+		MARKER.iconMaterial,
+		MARKER.ltcolor
+	)
 end)
